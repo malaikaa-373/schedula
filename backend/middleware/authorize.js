0d21ce8e8e6060ab1aef5b360d5068e72fc0f050
Malaika <malaikaa373@google.com>
2026-07-24 17:42:28 +0500

    authorize middleware for user's role

---
+++ backend/middleware/authorize.js
@@ -0,0 +1,15 @@
+const authorize = (allowedRoles) =>{
+    
+    return (req,res,next) =>{
+        //check the permission for user's role
+        if (!allowedRoles.includes(req.user.role)) {
+            return res
+            .status(403)
+            .json({message:"NO permission"})
+        }
+        next()
+    }
+}
+export{
+    authorize
+}
