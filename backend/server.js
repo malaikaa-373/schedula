9810ba08c7833e4f9620b1468551dd8287207acf
malaika-373 <malaikaa373@gmail.com>
2026-07-28 20:07:59 +0500

    add routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose";
 import { router as authRoutes } from "./routes/authRoutes.js"
+import {Router} from "express"
 
 dotenv.config()
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/business" , Router)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
