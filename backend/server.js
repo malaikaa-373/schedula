a25efd4cc8a9e6b416eb9e0f2a23334a2e4efdbc
malaika-373 <malaikaa373@gmail.com>
2026-07-28 16:45:10 +0500

    add routes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+app.use("/api/auth", authRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
