aa70f75ad593f6b5dbe15447e61ebbda70124a33
malaika-373 <malaikaa373@gmail.com>
2026-07-29 11:40:51 +0500

    add service routes

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createService , getServices , deleteService , updateService } from "../controllers/serviceController";
+import { createService , getServices , deleteService , updateService } from "../controllers/serviceController.js";
 import {auth} from "../middleware/authenticate.js"
 import { authorize } from "../middleware/authorize.js";
 
-const route = Router()
+const router = Router()
 
 router.post("/", auth, authorize(["admin"]), createService)
 router.get("/" , auth , getServices)
