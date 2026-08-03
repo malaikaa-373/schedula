2bfe81bbd84ec7d012018939941c5b30303fb7be
malaika-373 <malaikaa373@gmail.com>
2026-08-03 17:15:20 +0500

    add staff routes

---
+++ backend/routes/staffRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { addStaff, getStaff , updateStaff , deactivatStaff , updateAvailability } from "../controllers/staffController.js";
+import {auth} from "../middleware/authenticate.js"
+import { authorize } from "../middleware/authorize.js";
+
+const router = Router()
+
+router.post("/", auth, authorize(["admin"]), addStaff)
+router.put("/:id", auth, authorize(["admin"]), updateStaff)
+router.get("/", auth, getStaff)
+router.put("/:id/deactivate" , auth , authorize(["admin"]) , deactivatStaff)
+router.put("/:id/availability", auth , authorize (["admin"]) , updateAvailability)
+
+export{
+    router
+}
