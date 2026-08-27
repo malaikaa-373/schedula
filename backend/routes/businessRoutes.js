8cdf29e19acd4a4421cec9cb26d446f86fa8ebf4
malaika-373 <malaikaa373@gmail.com>
2026-08-27 12:50:13 +0500

    add business get/update routes

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {getBusiness , updateBusiness} from "../controllers/bussinessController.js"
+import {getBusiness , updateBusiness , createBusiness} from "../controllers/bussinessController.js"
 import {auth } from "../middleware/authenticate.js"
 import {authorize} from "../middleware/authorize.js"
 
@@ -7,6 +7,7 @@ const router = Router()
 
 router.get("/me", auth, getBusiness)
 router.put("/me", auth, authorize(["admin"]), updateBusiness)
+router.post("/create", auth, createBusiness);
 
 export{
     router
